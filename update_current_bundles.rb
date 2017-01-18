3575e17b2f1eea49376e65ce1bb418b491ff6731
Matthias Grundmann <matthias@glasmail.de>
2017-01-18 17:55:50 +0100

    Update bundle name detection

--- update_current_bundles.rb
+++ update_current_bundles.rb
@@ -3,7 +3,18 @@
 require 'nokogiri'
 require 'open-uri'
 
-home_page = open("https://www.humblebundle.com")
+require 'net/http'
+require 'openssl'
+
+def open_url(url)
+  uri = URI.parse(url)
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true
+  http.ssl_version = :TLSv1
+  return http.get(uri.path).body
+end
+
+home_page = open_url("https://www.humblebundle.com/")
 doc = Nokogiri::HTML(home_page)
 subtabs = doc.css("#subtab-container").css("a")
 
@@ -13,8 +24,8 @@ subtabs.each { |subtab|
 }
 
 def check_line(line)
-  if line.include? 'sales_counter(' then
-    line =~ /sales_counter\('([a-z0-9_-]*)'/
+  if line.include? "product_machine_name" then
+    line =~ /product_machine_name": "([a-z0-9_-]*)"/
     bundle = $1
     $output += bundle + "\n"
   end
@@ -23,12 +34,10 @@ end
 $output = ''
 
 urls.each { |url|
-  open("https://www.humblebundle.com" + url) { |f|
-    f.each_line { |line| check_line(line) }
-  }
+  page = open_url("https://www.humblebundle.com" + url)
+  page.each_line { |line| check_line(line) }
 }
 
-home_page.rewind
 home_page.each_line { |line| check_line(line) }
 
 File.write(ARGV[0] || './current_bundles', $output) unless $output == ''
